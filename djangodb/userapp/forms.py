97501931e588169fa136d25654a3a20b4285c8fa
Prajwal Bashyal <bashyalprajwal24@gmail.com>
2025-08-10 12:59:33 +0545

    usersignup form added

--- djangodb/userapp/forms.py
+++ djangodb/userapp/forms.py
@@ -1,7 +1,9 @@
 # userapp/forms.py
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import CustomUser, Profile, vendor
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Username', max_length=30, widget=forms.TextInput(attrs={
@@ -12,3 +14,12 @@ class LoginForm(AuthenticationForm):
         'class': 'form-control',
         'placeholder': 'Enter your password',
     }))
+
+
+class UserSignUpForm(UserCreationForm):
+    is_vendor = forms.BooleanField(required=False, label='Are you a vendor?', initial=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'is_vendor')
+
